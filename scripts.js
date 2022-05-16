7d714c2ec54bdc77435642ce5f239fb1820f0307
LeonardoFurmann <leohitnerf@gmail.com>
2022-05-16 08:59:18 -0300

    Primeira versão

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function atv_01_btn_01(){
+    window.alert("Você pressionou o botão 1");
+}
+
+function atv_01_btn_02(){
+    window.alert("Você pressionou o botão 2");
+}
+
+function atv_01_btn_03(){
+    window.alert("Você pressionou o botão 3");
+}
